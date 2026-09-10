4e21eb40fd3645906592c26f967dd6a7bc98dc24
Starlinjl <starlinstar35@gmail.com>
2026-09-10 12:56:38 +0530

    Initial commit with index.ts

--- Back/src/index.ts
+++ Back/src/index.ts
@@ -4,14 +4,11 @@ import cors from "cors";
 import dotenv from "dotenv";
 import postRoutes from "./routes/postRoutes";
 import authRoutes from "./routes/authRoutes";
-import commentRoutes from "./routes/commentRoutes"
+import commentRoutes from "./routes/commentRoutes";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-
-app.post
 
 app.use(cors());
 app.use(express.json());
@@ -25,16 +22,13 @@ app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-const startServer = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI as string);
+mongoose
+  .connect(process.env.MONGO_URI as string)
+  .then(() => {
     console.log("MongoDB Connected");
+  })
+  .catch((error) => {
+    console.log("MongoDB connection error:", error);
+  });
 
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  } catch (error) {
-    console.log("Error:", error);
-  }
-};
-startServer()
+export default app;
